52bd63f4beafe18537b1b510ef3eebc13de38bbd
Daniel Vitor <daniel.vit0r@outlook.com>
2021-02-24 16:58:15 -0300

    Adição de filtro por pesquisa e geração

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&display=swap" rel="stylesheet">
+
+    <div id='project-pokedex'>
+        <div class='container-title'>
+            <h1 class="title"> Pokedex </h1>
+            <p  class="subtitle"> 
+                Made with <strong>Vue</strong>.js and the <a href="">PokeApi.co</a> data! 
+            </p>
+        </div>
+        
+        
         <Pokedex /> 
+        
+        
     </div>
 </template>
 
@@ -19,5 +31,49 @@ export default {
 </script>
 
 <style>
+    .container-title{
+        margin-top: 5%;
+    }
 
+    .title{
+        text-align: center;
+        font-size: 45px;
+        font-family: 'Josefin Sans', sans-serif;
+    }
+
+    .title::after{
+        width: 200px;
+        height: 3px;
+        background: black;
+        display: block;
+        content: '';
+        margin: auto;
+        margin-bottom: 1%;
+    }
+
+    .subtitle{
+        text-align: center;
+        font-family: 'Josefin Sans', sans-serif;
+    }
+
+    .subtitle strong{
+        color: #41b883;
+    }
+
+    @media (min-width: 300px) and (max-width: 450px){
+        .subtitle{
+            margin-top: 2%;
+        }
+    }
+    .gen{
+        margin-top: 2%;
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .btn-gen{
+        width: 80px;
+        height: 40px;
+    }
 </style>
--- src/components/pokedex.vue
+++ src/components/pokedex.vue
@@ -1,27 +1,54 @@
 <template>
-  <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Arvo&display=swap"
+    rel="stylesheet"
+  />
+
+  <div class="gen">
+    <button class="btn-gen" @click="getPokemon(1, 151)">1ª Gen</button>
+    <button class="btn-gen" @click="getPokemon(152, 251)">2ª Gen</button>
+    <button class="btn-gen" @click="getPokemon(252, 386)">3ª Gen</button>
+    <button class="btn-gen" @click="getPokemon(387, 493)">4ª Gen</button>
+    <button class="btn-gen" @click="getPokemon(495, 649)">5ª Gen</button>
+    <button class="btn-gen" @click="getPokemon(650, 721)">6ª Gen</button>
+    <button class="btn-gen" @click="getPokemon(722, 809)">7ª Gen</button>
+    <button class="btn-gen" @click="getPokemon(810, 898)">8ª Gen</button>
+  </div>
+
+  <span class="searchpokemon">
+    <input id="searchpoke" type="text" placeholder="Pokemon Name" />
+    <button class="search" @click="getPokemonByName()">Pesquisar</button>
+  </span>
 
   <div id="pokedex">
     <div :key="poke.id" v-for="poke in pokemon" class="card">
       <h2 class="pokeName">{{ poke.name }}</h2>
-      
-      <div v-if="poke.isLegendary" class='legendary'>
+
+      <div v-if="poke.isLegendary" class="legendary">
         <p>Legendary</p>
       </div>
 
-      <div v-if="poke.isMythical" class='mythical'>
+      <div v-if="poke.isMythical" class="mythical">
         <p>Mythical</p>
       </div>
 
-      <img :src="poke.imgSrc" class="imgPoke">
-      
+      <img :src="poke.imgSrc" class="imgPoke" />
+
       <div class="stats">
-          <span :key='stat' v-for='stat in poke.stats'>
-            <img v-if="stat.stat == 'hp'" style="width: 20px;" src="../assets/heart.svg" alt="">
-            <img v-else src="../assets/swords.svg" style="width: 20px;" alt="">
-            <p> {{ stat.statValue }} </p>
-          </span>
+        <span :key="stat" v-for="stat in poke.stats">
+          <img
+            v-if="stat.stat == 'hp'"
+            style="width: 20px"
+            src="../assets/heart.svg"
+            alt=""
+          />
+          <img v-else src="../assets/swords.svg" style="width: 20px" alt="" />
+          <p>{{ stat.statValue }}</p>
+        </span>
       </div>
 
       <div class="type">
@@ -29,8 +56,6 @@
           {{ el.type.name }}
         </p>
       </div>
-
-      
     </div>
   </div>
 </template>
@@ -39,20 +64,71 @@
 export default {
   data() {
     return {
-      pokemon: []
+      pokemon: [],
     };
   },
   //* Function to get pokemon
-  async created(){
-    for(let index = 1; index <= 151; index++) {
-      let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}/`);
+  methods: {
+    async getPokemon(begin, end) {
+      //! 1ª Gen: 1 - 151
+      //! 2ª Gen: 152 - 251
+      //! 3ª Gen: 252 - 386
+      //! 4ª Gen: 387 - 493
+      //! 5ª Gen: 494 - 649
+      //! 6ª Gen: 650 - 721
+      //! 7ª Gen: 722 - 809
+      //! 8ª Gen: 810 - 898
+
+      this.pokemon = [];
+
+      for (let index = begin; index <= end; index++) {
+        let response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${index}/`
+        );
+        let data = await response.json();
+        console.log(data);
+
+        let response2 = await fetch(data.species.url);
+        let data2 = await response2.json();
+
+        console.log(data2);
+
+        this.pokemon.push({
+          name: data.name,
+          imgSrc: data.sprites.front_default,
+          types: data.types,
+          isLegendary: data2.is_legendary,
+          isMythical: data2.is_mythical,
+          stats: [
+            {
+              stat: data.stats[0].stat.name,
+              statValue: data.stats[0].base_stat,
+            },
+            {
+              stat: data.stats[1].stat.name,
+              statValue: data.stats[1].base_stat,
+            },
+          ],
+        });
+      }
+    },
+    async getPokemonByName() {
+      this.pokemon = [];
+
+      let name = document.getElementById("searchpoke").value;
+
+      console.log(name);
+      let response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}/`
+      );
       let data = await response.json();
+
       console.log(data);
 
       let response2 = await fetch(data.species.url);
-      let data2 = await response2.json();  
+      let data2 = await response2.json();
 
-      console.log(data2)
+      console.log(data2);
 
       this.pokemon.push({
         name: data.name,
@@ -63,62 +139,79 @@ export default {
         stats: [
           {
             stat: data.stats[0].stat.name,
-            statValue: data.stats[0].base_stat
+            statValue: data.stats[0].base_stat,
           },
           {
             stat: data.stats[1].stat.name,
-            statValue: data.stats[1].base_stat
-          }
-        ], 
-      })
-    }
-  }
+            statValue: data.stats[1].base_stat,
+          },
+        ],
+      });
+    },
+  },
+  async created() {
+    this.getPokemon(1, 3);
+  },
 };
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
 }
 
-.legendary{
-  position: absolute;
-  left: -5%;
-  top: 25%;
-  height: 50%;
+/* STYLES FOR THE GENERATION OPTIONS */
+.gen {
+  
+}
+
+@media (min-width: 300px) and (max-width: 450px) {
+  .gen {
+    flex-wrap: wrap;
+    width: 90%;
+  }
+
+  .btn-gen {
+    margin: 1%;
+  }
+
+  .card{
+    margin-bottom: 4% !important;
+    background-color:#5c95fc
+  }
+}
+
+.btn-gen {
+  font-family: "Josefin Sans", sans-serif;
+  background: none;
+  border: 1px solid black;
+}
+
+/* STYLES FOR THE SEARCH INPUT AND BUTTON */
+.searchpokemon {
+  width: 100%;
+  margin-top: 2%;
   display: flex;
-  text-align: center;
-  font-size: 10px;
+  justify-content: center;
 }
 
-.legendary p{
-  border-radius: 5px;
-  background-color: yellow;
-  writing-mode: vertical-lr;
-  text-orientation: upright;
-  font-family: 'DotGothic16', sans-serif;
+#searchpoke {
+  background: none;
+  border: 1px solid black;
+  padding: 5px;
+  font-family: "Josefin Sans", sans-serif;
 }
 
-.mythical{
-  position: absolute;
-  left: -5%;
-  top: 25%;
-  height: 50%;
-  display: flex;
-  text-align: center;
-  font-size: 10px;
+.search {
+  margin-left: 0.5%;
+  font-family: "Josefin Sans", sans-serif;
+  background: none;
+  border: 1px solid black;
+  padding: 2px;
 }
 
-.mythical p{
-  border-radius: 5px;
-  background-color: #1F0322;
-  color: white;
-  writing-mode: vertical-lr;
-  text-orientation: upright;
-  font-family: 'DotGothic16', sans-serif;
-}
-
+/* STYLES FOR THE POKEDEX */
 #pokedex {
   width: 90%;
   margin: auto;
@@ -126,21 +219,22 @@ export default {
   flex-wrap: wrap;
 }
 
+/* STYLES FOR THE CARD PROPERTIES */
 .card {
   width: 150px;
   height: 200px;
-  margin: 5%;
+  margin: 2%;
   border: 1px solid black;
   position: relative;
   display: grid;
   place-items: center;
 }
 
-.imgPoke{
-  position:absolute;
+.imgPoke {
+  position: absolute;
 }
 
-.pokeName{
+.pokeName {
   width: 100%;
   background: black;
   color: white;
@@ -148,12 +242,13 @@ export default {
   text-align: center;
   position: absolute;
   top: 0;
-  font-family: 'Arvo', serif;
+  font-family: "Josefin Sans", sans-serif;
   font-size: 20px;
   padding: 2px;
 }
 
-.stats{
+/* STYLES FOR STATUS */
+.stats {
   position: absolute;
   top: 75%;
   width: 100%;
@@ -163,114 +258,151 @@ export default {
   font-size: 15px;
 }
 
-.stats span{
+.stats span {
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.stats span p{
+.stats span p {
   color: black;
 }
 
-.type{
+/* STYLES FOR TYPE */
+.type {
   width: 100%;
   display: flex;
-  justify-content:space-around;
+  justify-content: space-around;
   position: absolute;
   bottom: -5%;
 }
 
-.type p{
+.type p {
   width: 40%;
   text-align: center;
   padding: 2px;
   text-transform: capitalize;
   border-radius: 2px;
-  font-family: 'DotGothic16', sans-serif;
+  font-family: "DotGothic16", sans-serif;
+}
+
+/* STYLES FOR THE RARITY */
+.legendary {
+  position: absolute;
+  left: -5%;
+  top: 25%;
+  height: 50%;
+  display: flex;
+  text-align: center;
+  font-size: 10px;
+}
+
+.legendary p {
+  border-radius: 5px;
+  background-color: yellow;
+  writing-mode: vertical-lr;
+  text-orientation: upright;
+  font-family: "DotGothic16", sans-serif;
+}
+
+.mythical {
+  position: absolute;
+  left: -5%;
+  top: 25%;
+  height: 50%;
+  display: flex;
+  text-align: center;
+  font-size: 10px;
+}
+
+.mythical p {
+  border-radius: 5px;
+  background-color: #1f0322;
+  color: white;
+  writing-mode: vertical-lr;
+  text-orientation: upright;
+  font-family: "DotGothic16", sans-serif;
 }
 
 /* STYLES FOR TYPES */
-
-.bug{
-    background: #afc421;
-    box-shadow: 3px 3px 3px #afc421;
+.bug {
+  background: #afc421;
+  box-shadow: 3px 3px 3px #afc421;
 }
 
-.dark{
-    background: #6b5142;
+.dark {
+  background: #6b5142;
 }
 
-.dragon{
-    background: #7a3dff;
+.dragon {
+  background: #7a3dff;
 }
 
-.electric{
-    background: #f6d434;
-    font-size: 14px;
+.electric {
+  background: #f6d434;
+  font-size: 14px;
 }
 
-.fighting{
-    background: #c62922;
-    font-size: 14px;
+.fighting {
+  background: #c62922;
+  font-size: 14px;
 }
 
-.fire{
-    background:#e98049;
+.fire {
+  background: #e98049;
 }
 
-.flying{
-    background: #967ed2
+.flying {
+  background: #967ed2;
 }
 
-.ghost{
-    background: #644c8a
+.ghost {
+  background: #644c8a;
 }
 
-.grass{
-    background: #7cc359
+.grass {
+  background: #7cc359;
 }
 
-.ground{
-    background: #debe5b
+.ground {
+  background: #debe5b;
 }
 
-.ice{
-    background: #9adbdd;
+.ice {
+  background: #9adbdd;
 }
 
-.normal{
-    background: #b6ba87;
+.normal {
+  background: #b6ba87;
 }
 
-.fairy{
-  background: #ff87ab
+.fairy {
+  background: #ff87ab;
 }
 
-.poison{
-    background: #a441ad;
-    box-shadow: 1px 1px 3px #6c2b72;
+.poison {
+  background: #a441ad;
+  box-shadow: 1px 1px 3px #6c2b72;
 }
 
-.psychic{
-    background: #ff417b
+.psychic {
+  background: #ff417b;
 }
 
-.rock{
-    background: #b59e2e
+.rock {
+  background: #b59e2e;
 }
 
-.steel{
-    background: #c1bfcd
+.steel {
+  background: #c1bfcd;
 }
 
-.water{
-    background: #5c95fc
+.water {
+  background: #5c95fc;
 }
 
-.shadow{
-    background: #7f6cbb
+.shadow {
+  background: #7f6cbb;
 }
-
 </style>
